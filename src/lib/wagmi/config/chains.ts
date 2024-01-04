1c12fe20dbf9e4751782340bbfb8f0701ff2cca4
Songkeys <songv587@gmail.com>
2024-01-04 11:29:31 +0800

    feat: add l2 info to chain definition

--- src/lib/wagmi/config/chains.ts
+++ src/lib/wagmi/config/chains.ts
@@ -3,6 +3,8 @@ import { mainnet, sepolia } from "wagmi/chains"
 
 import { env } from "@/env.mjs"
 
+const sourceId = sepolia.id
+
 export const rss3Testnet = /*#__PURE__*/ defineChain({
   id: 2_333,
   name: "RSS3 Chain Testnet",
@@ -30,8 +32,30 @@ export const rss3Testnet = /*#__PURE__*/ defineChain({
       address: "0x4451b8F89A4E352a8dA927705659f404D9651cF4",
       blockCreated: 783722,
     },
+    gasPriceOracle: { address: "0x420000000000000000000000000000000000000F" },
+    l1Block: { address: "0x4200000000000000000000000000000000000015" },
+    l2CrossDomainMessenger: {
+      address: "0x4200000000000000000000000000000000000007",
+    },
+    l2Erc721Bridge: { address: "0x4200000000000000000000000000000000000014" },
+    l2StandardBridge: { address: "0x4200000000000000000000000000000000000010" },
+    l2ToL1MessagePasser: {
+      address: "0x4200000000000000000000000000000000000016",
+    },
+    l2OutputOracle: {
+      [sourceId]: {
+        address: "0xf8523B3C14a9F3F9C768995eDDC7fF90cFB972b6",
+      },
+    },
+    portal: {
+      [sourceId]: {
+        address: "0x22458503eF9fa318BE18166C79B29C10063C6b37",
+        blockCreated: 4864634,
+      },
+    },
   },
   testnet: true,
+  sourceId,
 })
 
 const appEnv = env.NEXT_PUBLIC_ENV
