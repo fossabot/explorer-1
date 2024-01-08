1a21df9974d30e9aeb071c24a527a7ded0405d22
Songkeys <songv587@gmail.com>
2024-01-08 22:26:04 +0800

    fix: token addresses

--- src/lib/wagmi/config/tokens.ts
+++ src/lib/wagmi/config/tokens.ts
@@ -11,12 +11,12 @@ export const rss3Tokens: {
 } = {
   decimals: 18,
   [sepolia.id]: {
-    address: "0xd892f9f0eea6f6a586aaba7ed895a6eb4fa754d0",
+    address: "0x568F64582A377ea52d0067c4E430B9aE22A60473",
   },
   [mainnet.id]: {
     address: "0xc98d64da73a6616c42117b582e832812e7b8d57f",
   },
   [rss3Testnet.id]: {
-    address: "0x4200000000000000000000000000000000000301",
+    address: "0x4200000000000000000000000000000000000042",
   },
 }
